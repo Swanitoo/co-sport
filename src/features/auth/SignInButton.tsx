af3ef6b61e34acb0992debdeb3158af4a87a52cf
Swan <swan.marin@epitech.eu>
2024-04-19 14:50:08 +0200

    auth completed

--- src/features/auth/SignInButton.tsx
+++ src/features/auth/SignInButton.tsx
@@ -1,5 +1,6 @@
 import { signIn } from "@/auth/auth";
 import { Button } from "@/components/ui/button";
+import { LogIn } from "lucide-react";
 
 export const SignInButton = () => {
     return (
@@ -12,6 +13,7 @@ export const SignInButton = () => {
                     await signIn();
                 }}
             >
+                <LogIn size={16} className="mr-2" />
                 Sign In
             </Button>
         </form>
