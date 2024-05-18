64686151e8cf752aaa0a401a31cee6cf5315c977
Swan <swan.marin@epitech.eu>
2024-05-18 14:34:30 +0200

    step 2

--- app/(user)/r/[slug]/SocialSelector.tsx
+++ app/(user)/r/[slug]/SocialSelector.tsx
@@ -5,11 +5,23 @@ import { useState } from "react";
 
 export const SocialSelector = () => {
     const [url, setUrl] = useState("");
+    const [name, setName] = useState("");
 
     return (
         <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-                <Input className="bg-background/50" />
+            <div className="flex flex-col items-center gap-2">
+                <Input 
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="bg-background/50" 
+                    placeholder="Name"
+                />
+                <Input 
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                    className="bg-background/50" 
+                    placeholder="https://www.instagram.com/yourinstagram/"
+                />
                 <Button>
                     <Check size={16} />
                 </Button>
