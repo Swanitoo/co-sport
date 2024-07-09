cc3eb7a081109e0d5fcc34a4d968df37f95d6d3f
Swan <swan.marin@gmail.com>
2024-07-09 17:56:56 +0200

    review text

--- app/(user)/r/[slug]/ReviewTextSelector.tsx
+++ app/(user)/r/[slug]/ReviewTextSelector.tsx
@@ -1,13 +1,30 @@
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
+import { useState } from "react";
 
 export type ReviewTextSelectorProps = {};
 
 export const ReviewTextSelector = (props: ReviewTextSelectorProps) => {
     return (
         <div className="grid w-full gap-2">
-          <Textarea placeholder="Type your message here." />
-          <Button>Send message</Button>
+            <InputControl />
         </div>
       )
+}
+
+const InputControl = ({}) => {
+    const [input, setInput] = useState("");
+    return (
+        <div className="flex flex-col gap-2">
+            <Textarea
+                placeholder="Write your review here"
+                className="w-full bg-accent/50"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <Button variant="default" size="sm" onClick={() => console.log(input)}>
+                Submit
+            </Button>
+        </div>
+    )
 }
