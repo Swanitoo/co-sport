86c3f34ec8f441c852a9876fb5dad122f0b5a1ba
Swan <swan.marin@gmail.com>
2024-07-10 00:12:28 +0200

    link review/wall

--- app/(customer)/products/[productId]/page.tsx
+++ app/(customer)/products/[productId]/page.tsx
@@ -1,9 +1,11 @@
 import { requieredCurrentUser } from "@/auth/current-user";
 import { Layout, LayoutTitle } from "@/components/layout";
+import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { prisma } from "@/prisma";
 import type { PageParams } from "@/types/next";
+import { Link2 } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
@@ -42,8 +44,26 @@ export default async function RoutePage(
           <CardHeader>
             <CardTitle>Details</CardTitle>
           </CardHeader>
-          <CardContent>
+          <CardContent className="flex flex-col gap-2 items-start">
             <p>Slug : {product.slug}</p>
+            <Link
+              href={`/r/${product.slug}`}
+              className={buttonVariants({
+                size: "sm",
+              })}
+            >
+              <Link2 size={16} className="mr-2" />
+              Share review link
+            </Link>
+            <Link
+              href={`/wall/${product.slug}`}
+              className={buttonVariants({
+                size: "sm",
+              })}
+            >
+              <Link2 size={16} className="mr-2" />
+              Wall link
+            </Link>
           </CardContent>
         </Card>
         <Card className="flex-1">
