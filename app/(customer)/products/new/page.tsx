d5da4a6bda9a171a1a09f2489373142dd0a98641
Swan <swan.marin@gmail.com>
2024-07-10 20:08:14 +0200

    plan premium alert

--- app/(customer)/products/new/page.tsx
+++ app/(customer)/products/new/page.tsx
@@ -1,8 +1,34 @@
 import { Layout, LayoutTitle } from "@/components/layout";
 import type { PageParams } from "@/types/next";
 import { ProductForm } from "../[productId]/edit/ProductForm";
+import { requieredCurrentUser } from "@/auth/current-user";
+import { prisma } from "@/prisma";
+import { AlertTriangle } from "lucide-react";
 
 export default async function RoutePage(props: PageParams<{}>) {
+
+    const user = await requieredCurrentUser();
+
+    const isAutorized =
+        user.plan === "PREMIUM"
+            ? true
+            : (await prisma.product.count({
+                where: {
+                    userId: user.id,
+                },
+            })) < 1;
+
+    if (!isAutorized) {
+        return (
+            <Layout>
+                <LayoutTitle>Create product</LayoutTitle>
+                <p>
+                    <AlertTriangle className="inline" />
+                    Sorry, you need to upgrade to our premium plan to create more products.
+                </p>
+            </Layout>
+        )
+    }
     return (
         <Layout>
             <LayoutTitle>
