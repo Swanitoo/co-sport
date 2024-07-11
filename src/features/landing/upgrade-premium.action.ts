608bea01bd4962857ac9c40c9256ce98bd4fc7d7
Swan <swan.marin@gmail.com>
2024-07-11 08:10:42 +0200

    integration stripe

--- src/features/landing/upgrade-premium.action.ts
+++ src/features/landing/upgrade-premium.action.ts
@@ -2,5 +2,12 @@ import { userAction } from "@/safe-actions";
 import { z } from "zod";
 
 export const upgradeToPremium = userAction(z.string(), async (_ , context) => {
+    z.string(),
+    async (_, context) => {
+        const stripeCustomerId = context.user.stripeCustomerId;
 
+        if (!stripeCustomerId){
+            throw new Error("User does not have a stripe customer Id");
+        }
+    }
 })
