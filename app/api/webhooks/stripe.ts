455f7565462dde46926b3fdb70b08bf466f18dea
Swan <swan.marin@gmail.com>
2024-07-13 19:51:07 +0200

    integration stripe

--- app/api/webhooks/stripe.ts
+++ app/api/webhooks/stripe.ts
@@ -1,4 +1,5 @@
 import { env } from "@/env";
+import { prisma } from "@/prisma";
 import { stripe } from "@/stripe";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
@@ -22,5 +23,32 @@ export const POST = async (req: NextRequest) => {
         );
     } catch (err) {
         return NextResponse.json({ error: "Invalid stripe signature" }, { status: 400});
+    }
+
+    switch (event.type) {
+        case "checkout.session.completed": {
+            const session = event.data.object as Stripe.Checkout.Session;
+            const customerId = session.customer as string;
+
+            const user = await prisma.user.findFirst({
+                where: {
+                    stripeCustomerId: customerId,
+                },
+            });
+
+            if (!user) {
+                return NextResponse.json({ error: "User not found" }, {status: 404});
+            }
+
+            await prisma.user.update({
+                where: {
+                    id: user.id,
+                },
+                data: {
+                    plan: "PREMIUM",
+                },
+            });
+
+        }
     }
 };
