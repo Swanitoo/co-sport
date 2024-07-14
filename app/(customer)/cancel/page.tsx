34ade842eb45bb7b80a1363153ab233e6bbbea23
Swan <swan.marin@gmail.com>
2024-07-14 16:31:08 +0200

    payment cancel page

--- app/(customer)/cancel/page.tsx
+++ app/(customer)/cancel/page.tsx
@@ -1,11 +1,28 @@
-import type { PageParams } from "@/types/next";
 import { Layout, LayoutTitle } from "@/components/layout";
+import { buttonVariants } from "@/components/ui/button";
+import type { PageParams } from "@/types/next";
+import Link from "next/link";
 
 export default async function RoutePage(props: PageParams<{}>) {
-
   return (
     <Layout>
-        <LayoutTitle>Ohhh... it's seems the payment has been canceled</LayoutTitle>
+      <LayoutTitle>
+        Ohhh... it's seems the payment has been canceled
+      </LayoutTitle>
+      <div className="flex gap-4">
+        <Link
+          className={buttonVariants({ size: "lg", variant: "secondary" })}
+          href="/"
+        >
+          Back to home
+        </Link>
+        <Link
+          className={buttonVariants({ size: "lg" })}
+          href="mailto:swan.marin@gmail.com"
+        >
+          Contact help
+        </Link>
+      </div>
     </Layout>
   );
 }
