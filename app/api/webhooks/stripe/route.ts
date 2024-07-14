211277304abce271c6bcab43f87d88fce1962bbb
Swan <swan.marin@gmail.com>
2024-07-14 17:04:52 +0200

    remove premium plan

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -5,57 +5,102 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export const POST = async (req: NextRequest) => {
-    const body = await req.text();
+  const body = await req.text();
 
-    const stripeSignature = req.headers.get("stripe-signature");
+  const stripeSignature = req.headers.get("stripe-signature");
 
-    if (!stripeSignature)  {
-        return NextResponse.json({ error: "No stripe signature" }, { status: 400});
+  if (!stripeSignature) {
+    return NextResponse.json({ error: "No stripe signature" }, { status: 400 });
+  }
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      stripeSignature,
+      env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Invalid stripe signature" },
+      { status: 400 }
+    );
+  }
+
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    const customerId = session.customer as string;
+
+    const user = await prisma.user.findFirst({
+      where: {
+        stripeCustomerId: customerId,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
 
-    let event: Stripe.Event;
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        plan: "PREMIUM",
+      },
+    });
+  } else if (event.type === "invoice.paid") {
+    // continue to set the user premium
+    const invoice = event.data.object as Stripe.Invoice;
 
-    try {
-        event = stripe.webhooks.constructEvent(
-            body,
-            stripeSignature,
-            env.STRIPE_WEBHOOK_SECRET,
-        );
-    } catch (err) {
-        return NextResponse.json({ error: "Invalid stripe signature" }, { status: 400});
+    const customerId = invoice.customer as string;
+
+    const user = await prisma.user.findFirst({
+      where: {
+        stripeCustomerId: customerId,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
 
-    switch (event.type) {
-        case "checkout.session.completed": {
-            const session = event.data.object as Stripe.Checkout.Session;
-                console.log(session)
-                const customerId = session.customer as string;
-            
-                const user = await prisma.user.findFirst({
-                    where: {
-                        stripeCustomerId: customerId,
-                    },
-                });
-    
-                if (!user) {
-                    return NextResponse.json({ error: "User not found" }, {status: 404});
-                }
-    
-                await prisma.user.update({
-                    where: {
-                        id: user.id,
-                    },
-                    data: {
-                        plan: "PREMIUM",
-                    },
-                });
-            
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        plan: "PREMIUM",
+      },
+    });
+  } else if (event.type === "customer.subscription.deleted") {
+    // Remove the premium plan
+    const subscription = event.data.object as Stripe.Subscription;
 
-            break;
-            }
-        default: {
-            console.log("Unhandled event", event);
-        }
+    const customerId = subscription.customer as string;
+
+    const user = await prisma.user.findFirst({
+      where: {
+        stripeCustomerId: customerId,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
-    return NextResponse.json({ reveived: true });
+
+
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        plan: "FREE",
+      },
+    });
+  }
+
+  return NextResponse.json({ received: true });
 };
