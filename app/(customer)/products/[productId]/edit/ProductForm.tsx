9ccfc7063deea365187af568d6b691ef9a3ebf72
Swan <swan.marin@gmail.com>
2024-07-21 19:35:10 +0200

    meta-data

--- app/(customer)/products/[productId]/edit/ProductForm.tsx
+++ app/(customer)/products/[productId]/edit/ProductForm.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  GRADIENTS_CLASSES,
-  ProductSchema,
-  ProductType,
-} from "./product.schema";
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useZodForm,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
   Select,
@@ -14,16 +21,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, useZodForm } from "@/components/ui/form";
-import { useMutation } from "@tanstack/react-query";
-import { createProductAction, updateProductAction } from "./product.action";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { uploadImageAction } from "@/features/upload/upload.action";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { createProductAction, updateProductAction } from "./product.action";
+import {
+  GRADIENTS_CLASSES,
+  ProductSchema,
+  ProductType,
+} from "./product.schema";
 
 export type ProductFormProps = {
   defaultValues?: ProductType;
@@ -40,15 +50,15 @@ export const ProductForm = (props: ProductFormProps) => {
 
   const mutation = useMutation({
     mutationFn: async (values: ProductType) => {
-      const { data, serverError } = isCreate 
-      ? await createProductAction(values) : 
-      await updateProductAction({
-        id: props.productId ?? "-",
-        data: values,
-      });
+      const { data, serverError } = isCreate
+        ? await createProductAction(values)
+        : await updateProductAction({
+            id: props.productId ?? "-",
+            data: values,
+          });
 
       if (serverError || !data) {
-        throw new Error(serverError);
+        toast.error(serverError);
         return;
       }
 
@@ -90,92 +100,104 @@ export const ProductForm = (props: ProductFormProps) => {
             await mutation.mutateAsync(values);
           }}
         >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                    <Input placeholder="Sceance avec patoche" {...field} />
-                </FormControl>
-                <FormDescription>
-                  The name of the product ro review
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="image"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Image</FormLabel>
-                <div className="flex items-center gap-4">
-                  <FormControl className="flex-1">
-                      <Input type="file" placeholder="Sceance avec patoche" onChange={e => {
-                        const file = e.target.files?.[0];
+          <Tabs defaultValue="general">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="general">General</TabsTrigger>
+              <TabsTrigger value="texts">Text</TabsTrigger>
+            </TabsList>
+            <TabsContent className="flex flex-col gap-6" value="general">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Sceance bras" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      The name of the product to review
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Image</FormLabel>
 
-                        if (!file) {
-                          return;
-                        }
+                    <div className="flex items-center gap-4">
+                      <FormControl className="flex-1">
+                        <Input
+                          type="file"
+                          placeholder="sceance bras"
+                          onChange={(e) => {
+                            const file = e.target.files?.[0];
 
-                        if (file.size > 1024 * 1024) {
-                          toast.error("File is too big");
-                          return;
-                        }
+                            if (!file) {
+                              return;
+                            }
 
-                        if (!file.type.includes("image")) {
-                          toast.error("File is not an image");
-                          return;
-                        }
+                            if (file.size > 1024 * 1024) {
+                              toast.error("File is too big");
+                              return;
+                            }
 
-                        submitImage.mutate(file);
-                      }} 
+                            if (!file.type.includes("image")) {
+                              toast.error("File is not an image");
+                              return;
+                            }
+
+                            submitImage.mutate(file);
+                          }}
+                        />
+                      </FormControl>
+                      {submitImage.isPending ? (
+                        <Loader2 className="h-6 animate-spin" />
+                      ) : null}
+                      {field.value ? (
+                        <Avatar className="rounded-sm">
+                          <AvatarFallback>{field.value[0]}</AvatarFallback>
+                          <AvatarImage src={field.value} />
+                        </Avatar>
+                      ) : null}
+                    </div>
+                    <FormDescription>
+                      The name of the product to review
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="slug"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Slug</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Sceance bras"
+                        {...field}
+                        onChange={(e) => {
+                          const value = e.target.value
+                            .replaceAll(" ", "-")
+                            .toLowerCase();
+
+                          field.onChange(value);
+                        }}
                       />
-                  </FormControl>
-                  {submitImage.isPending ? (
-                    <Loader2 className="animate-spin h-6" />
-                  ) : null}
-                  {field.value ? (
-                    <Avatar>
-                      <AvatarImage src={field.value} />
-                    </Avatar>
-                  ) : null}
-                </div>
-                <FormDescription>
-                  The name of the product ro review
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="slug"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Slug</FormLabel>
-                <FormControl>
-                    <Input 
-                      placeholder="sceance-avec-patoche" 
-                      {...field} 
-                      onChange={(e) => {
-                        const value = e.target.value
-                        .replaceAll(" ", "-")
-                        .toLowerCase();
-
-                        field.onChange(value);
-                      }} />
-                </FormControl>
-                <FormDescription>
-                  The slug is used in the URL of the review page
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                    </FormControl>
+                    <FormDescription>
+                      The slug is used in the URL of the review page.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <FormField
                 control={form.control}
                 name="backgroundColor"
@@ -215,6 +237,97 @@ export const ProductForm = (props: ProductFormProps) => {
                   </FormItem>
                 )}
               />
+            </TabsContent>
+            <TabsContent className="flex flex-col gap-6" value="texts">
+              <FormField
+                control={form.control}
+                name="noteText"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Note Text</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Give a note / 5"
+                        {...field}
+                        value={field.value ?? ""}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      The title where the user can add a note / 5.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="informationText"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Information label</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="Please give us some information"
+                        {...field}
+                        value={field.value ?? ""}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      The label where the user give his name and social link.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="reviewText"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Review text</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="What you think about our product ?"
+                        {...field}
+                        value={field.value ?? ""}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      The label where the user can speak or write about the
+                      product.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="thanksText"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Thanks label</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="Thanks you for your review !"
+                        {...field}
+                        value={field.value ?? ""}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      The label when the user submit his review.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </TabsContent>
+          </Tabs>
+
           <Button>{isCreate ? "Create product" : "Save product"}</Button>
         </Form>
       </CardContent>
