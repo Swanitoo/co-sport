f455ffc61269fc7c0954a6abb8218dcc42ad8112
Swan <swan.marin@gmail.com>
2024-07-21 16:53:29 +0200

    CTA-section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { CTASection } from "@/features/landing/CTASection";
 import { FAQSection } from "@/features/landing/FAQSection";
 import { FeatureSection } from "@/features/landing/FeatureSection";
 import { HeroSection } from "@/features/landing/HeroSection";
@@ -17,6 +18,7 @@ export default function Home() {
       <ProblemsSection />
       <PricingSection />
       <FAQSection />
+      <CTASection />
     </div>
   );
 }
