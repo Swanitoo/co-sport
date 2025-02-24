d267fa6c436e18acc6d6f79b9675faf006b9f3dd
Swan <swan.marin@gmail.com>
2025-02-24 15:52:27 +0100

    fix: remove duplicate migration

--- app/(customer)/products/[productId]/edit/product.schema.ts
+++ app/(customer)/products/[productId]/edit/product.schema.ts
@@ -52,6 +52,7 @@ export const SPORTS = [
   { name: "Musculation", icon: "🏋️" },
   { name: "Natation", icon: "🏊" },
   { name: "Paddle", icon: "🏄" },
+  { name: "Pétanque", icon: "⚫" },
   { name: "Plongée", icon: "🤿" },
   { name: "Randonnée", icon: "🥾" },
   { name: "Roller", icon: "⛸️" },
