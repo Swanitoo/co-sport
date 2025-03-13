56c0c6ed74df9298adefe96dbfedbe36eb165066
Swan <swan.marin@gmail.com>
2025-03-13 20:24:18 +0100

    [FIX] - correct migration state for user id

--- prisma/migrations/20250312215500_fix_migration_state/migration.sql
+++
@@ -1,6 +0,0 @@
--- DropFailed
-DELETE FROM "_prisma_migrations" WHERE migration_name = '20250312213152_add_default_id_to_user';
-
--- ReapplyCorrectState
-ALTER TABLE "User" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
-ALTER TABLE "User" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP; 
